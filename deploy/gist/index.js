13fa6ef436d978b3e7e669c97e002e9063036598
khai96_ <hvksmr1996@gmail.com>
2016-10-05 17:55:07 +0700

    Complete deploy gist

--- deploy/gist/index.js
+++ deploy/gist/index.js
@@ -1,5 +1,13 @@
 'use strict'
 
+const NOW = new Date()
+
+const {join, parse} = require('path')
+
+const {readFileSync, readdirSync} = require('fs')
+
+const GitHubAPIs = require('github')
+
 const {
   env: {
     GIST_TOKEN,
@@ -20,3 +28,58 @@ if (!GIST_ID || !GIST_TOKEN) {
   stderr.write('Missing environment variables\n')
   exit(1)
 }
+
+const github = new GitHubAPIs()
+const files = {}
+const ENCODING = {encoding: 'utf8'}
+const CONTAINER = join(__dirname, '..', '..', 'out')
+const LIST = readdirSync(CONTAINER)
+
+const filename = item => {
+  const {name, extname} = parse(item)
+  return name + '-' + PROJECT_VERSION + extname
+}
+
+for (const item of LIST) {
+  const content = readFileSync(join(CONTAINER, item), ENCODING)
+  files[filename(item)] = {content}
+}
+
+github.authenticate({
+  type: 'token',
+  token: GIST_TOKEN
+})
+
+new Promise(
+  (resolve, reject) => {
+    github.gists.edit({
+      id: GIST_ID,
+      files
+    })
+      .then(
+        response => {
+          const rfiles = response.files
+          let summary = `# Release ${PROJECT_VERSION} - ${NOW.toISOString()}\n\n`
+          for (const item of LIST) {
+            const {raw_url, type, language} = rfiles[filename(item)]
+            summary += ` * [${item}](${raw_url})\n  - Type: ${type}\n  - Language: ${language}\n\n`
+          }
+          github.gists.edit({
+            id: GIST_ID,
+            files: {
+              [ filename('summary.md') ]: summary
+            }
+          })
+            .then(resolve)
+            .catch(reject)
+        }
+      )
+      .catch(reject)
+  }
+)
+  .catch(
+    () => {
+      stderr.write('Upload to Gist failed.\n')
+      exit(2)
+    }
+  )
