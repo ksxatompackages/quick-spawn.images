baf6a6f5f224bb406b4cb2b3c70beee795f2ebb0
khai96_ <hvksmr1996@gmail.com>
2016-10-08 13:15:44 +0700

    Sync Promise-Deployment uniform

--- deploy/gist/index.js
+++ deploy/gist/index.js
@@ -16,46 +16,44 @@ const {
     GIST_TOKEN,
     GIST_ID,
     GIT_REPO_TAG
-  },
-  exit,
-  stdout,
-  stderr
+  }
 } = require('process')
 
-if (!RELEASE_GIST) {
-  stdout.write('Skipped Gist release\n')
-  exit(0)
-}
+const {info, error} = global.console
 
-if (!GIST_ID || !GIST_TOKEN) {
-  stderr.write('Missing environment variables\n')
-  exit(1)
-}
-
-const github = new GitHubAPIs()
-const files = {}
-const ENCODING = {encoding: 'utf8'}
-const CONTAINER = join(__dirname, '..', '..', 'out')
-const LIST = readdirSync(CONTAINER)
-
-const filename = item => {
-  const {name, ext} = parse(item)
-  return name + '-' + GIT_REPO_TAG + ext
-}
-
-for (const item of LIST) {
-  const content = readFileSync(join(CONTAINER, item), ENCODING)
-  files[filename(item)] = {content}
-}
-
-github.authenticate({
-  type: 'token',
-  token: GIST_TOKEN
-})
 
 co(main)
 
 function * main () {
+  if (!RELEASE_GIST) {
+    info('Skipped Gist release')
+    error('Set RELEASE_GIST to TRUE if you want to deploy Gist Release schedule')
+    return Promise.resolve()
+  }
+  if (!GIST_ID || !GIST_TOKEN) {
+    const {EnvironmentVariableError} = require('../lib/error.js')
+    const message = 'Missing GIST_ID or GIST_TOKEN'
+    const response = new EnvironmentVariableError(message)
+    error(response)
+    return Promise.reject(response)
+  }
+  const github = new GitHubAPIs()
+  const files = {}
+  const ENCODING = {encoding: 'utf8'}
+  const CONTAINER = join(__dirname, '..', '..', 'out')
+  const LIST = readdirSync(CONTAINER)
+  const filename = item => {
+    const {name, ext} = parse(item)
+    return name + '-' + GIT_REPO_TAG + ext
+  }
+  for (const item of LIST) {
+    const content = readFileSync(join(CONTAINER, item), ENCODING)
+    files[filename(item)] = {content}
+  }
+  github.authenticate({
+    type: 'token',
+    token: GIST_TOKEN
+  })
   const artifactsResponse = yield github.gists.edit({
     id: GIST_ID,
     files
