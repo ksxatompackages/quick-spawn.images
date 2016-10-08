b8964ceb2d1bd3a37126629c73fb93a066b36f9e
khai96_ <hvksmr1996@gmail.com>
2016-10-08 19:07:48 +0700

    Create preview on test

--- sh/build.sh
+++ sh/build.sh
@@ -3,4 +3,5 @@ source ./sh/init-env.sh || exit 1
 ( [ -z $DEFAULT_BUILD_SCRIPT ] || bash -c "$DEFAULT_BUILD_SCRIPT" > ./out/$DEFAULT_BUILD_SCRIPT_PRODUCT ) &
 ( [ -z $DEFAULT_BUILD_URL ] || ( curl -L $DEFAULT_BUILD_URL > ./script.tmp && ./script.tmp > ./out/$DEFAULT_BUILD_URL_PRODUCT ) ) &
 node ./build
+[ "$BUILD_MODE" == "Debug" ] && npm run preview
 echo 'Done'
