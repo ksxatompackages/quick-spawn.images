d7c3ffd8a9a204a5f6feec1112a16ea8a07a9a43
khai96_ <hvksmr1996@gmail.com>
2016-10-09 00:10:18 +0700

    Simplify previewer invocation

--- sh/preview.sh
+++ sh/preview.sh
@@ -1,11 +1,9 @@
 node ./sh/preview.js && (
-  {
-    [ -f ./sh/previewer.sh ] &&
-    export PREVIEWER=$(./sh/previewer.sh) &&
-    [ ! -z $PREVIEWER ]
-  } && (
-    ( "$PREVIEWER" .preview.html || echo '(WARNING) Preview failed' >&2 ) &
+  [ -f ./sh/previewer.sh ] && (
+    (
+      ./sh/previewer.sh .preview.html && echo '(WARNING) Preview failed'
+    ) &
   ) || (
-    echo '(WARNING) No previewer specified' >&2
+    echo '(WARNING) No previewer specified'
   )
 )
