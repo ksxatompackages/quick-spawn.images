86ed5a0d28bce174b84973eec8cc3aa7499e2006
khai96_ <hvksmr1996@gmail.com>
2016-10-08 19:04:53 +0700

    Add preview creator

---
+++ sh/preview.js
@@ -0,0 +1,18 @@
+'use strict'
+const {join} = require('path')
+const {readdirSync, readFileSync, writeFileSync} = require('fs')
+const ENCODING = {encoding: 'utf8'}
+const create = require('pug')
+  .compile(readFileSync(join(__dirname, 'lib', 'preview.pug'), ENCODING), {pretty: '\x20\x20'})
+const source = join(__dirname, '..', 'out')
+const list = readdirSync(source)
+  .map(name => ({name, file: join(source, name)}))
+const style = {
+  'font-family': require('../dev-lib/font-family.js')
+}
+writeFileSync(
+  join(__dirname, '..', '.preview.html'),
+  create({list, html: {style}}),
+  ENCODING
+)
+console.log('Done')
