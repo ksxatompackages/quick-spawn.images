d9eb64ebccf91516e2bc9417dd138ba05b7a4a95
khai96_ <hvksmr1996@gmail.com>
2016-10-09 12:54:43 +0700

    Restruct: Extensibility  * Allowing pug files to use global JS resources, e.g. require

--- build/index.js
+++ build/index.js
@@ -20,6 +20,10 @@ const scripts = readdirSync(SCRIPTS)
     ([script, {name}]) =>
       [require(script), name]
   )
+  .map(
+    ([local, name]) =>
+      [{local, global, name, require, __dirname, __filename}, name]
+  )
 
 BUILD_MODE === 'Debug' || write(false, 'min')
 write('\x20\x20', 'pretty')
