ecb6873a242c5ad9d8d9b25fccb0881389c0f46e
khai96_ <hvksmr1996@gmail.com>
2016-10-10 18:03:50 +0700

    Deploy by Commits

--- deploy/github-target-repo/index.js
+++ deploy/github-target-repo/index.js
@@ -23,7 +23,9 @@ const GitHubAPIs = require('github')
 
 const {info, error} = global.console
 
-const ENCODING = {encoding: 'base64'}
+const encoding = 'utf8'
+const ENCODING = {encoding}
+const FILEBLOB = 100644
 
 const handle = (
   TARGET_GITHUB_RELEASE_OAUTH && TARGET_GITHUB_REPO_OWNER && TARGET_GITHUB_REPO_NAME && TARGET_GITHUB_REPO_DIRECTORY && ARTIFACTS_DIRECTORY
@@ -61,26 +63,62 @@ function * main () {
     type: 'token',
     token: TARGET_GITHUB_RELEASE_OAUTH
   })
-  const list = readdirSync(ARTIFACTS_DIRECTORY)
-    .map(
-      item =>
-        (resolve, reject) => {
-          const callback = (error, content) =>
-            error ? reject(error) : resolve({content, item})
-          readFile(join(ARTIFACTS_DIRECTORY, item), ENCODING, callback)
-        }
-    )
-    .map(
-      fn =>
-        new Promise(fn)
-    )
   const user = TARGET_GITHUB_REPO_OWNER
   const repo = TARGET_GITHUB_REPO_NAME
   const branch = TARGET_GITHUB_REPO_BRANCH
-  for (const promise of list) {
-    const {content, item} = yield promise
-    const path = join(TARGET_GITHUB_REPO_DIRECTORY, item)
-    const message = `Update /${path} to ${GIT_REPO_TAG}\n * Branch: ${branch}\n * Done automatically`
-    yield github.repos.createFile({user, repo, branch, path, content, message})
-  }
+  const ref = `heads/${branch}`
+  const {object: {sha: baseSHA}} = yield github.gitdata.getReference({user, repo, ref})
+  const parents = [baseSHA]
+  const {sha: baseTreeSHA} = yield github.gitdata.getTree({user, repo, sha: baseSHA})
+  const list = readdirSync(ARTIFACTS_DIRECTORY)
+    .map(
+      item => [
+        item,
+        new Promise(
+          (resolve, reject) => {
+            const callback = (error, content) =>
+              error ? reject(error) : resolve(content)
+            readFile(join(ARTIFACTS_DIRECTORY, item), ENCODING, callback)
+          }
+        )
+      ]
+    )
+    .map(
+      ([item, promise]) => [
+        item,
+        new Promise(
+          (resolve, reject) => {
+            const onfulfill = content =>
+              github.gitdata.createBlob({user, repo, content, encoding})
+                .then(resolve, reject)
+            promise.then(onfulfill, reject)
+          }
+        )
+      ]
+    )
+    .map(
+      ([item, promise]) => new Promise(
+        (resolve, reject) => promise.then(({sha}) => resolve({item, sha}), reject)
+      )
+    )
+  const listreponse = yield Promise.all(list)
+  const {sha: tree} = github.gitdata.createTree({
+    tree: listreponse.map(
+      ({item, sha}) => ({
+        path: join(TARGET_GITHUB_REPO_DIRECTORY, item),
+        blob: FILEBLOB,
+        type: 'blob',
+        base_tree: baseTreeSHA,
+        sha,
+        __proto__: null
+      })
+    ),
+    user,
+    repo,
+    __proto__: null
+  })
+  const message = (
+    `Update /${TARGET_GITHUB_REPO_DIRECTORY} to ${GIT_REPO_TAG}\n * Branch: ${repo}\n * Done automatically`
+  )
+  yield github.gitdata.createCommit({user, repo, message, tree, parents})
 }
